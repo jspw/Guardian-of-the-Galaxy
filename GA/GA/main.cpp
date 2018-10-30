b75691c9ac2d208ad02d1e6696d5de362556272d
Vampire_5 <mhshifat757@gmail.com>
2018-10-30 21:21:54 +0600

    backgroun and spaceship

--- GA/GA/main.cpp
+++ GA/GA/main.cpp
@@ -9,12 +9,28 @@
 using namespace sf;
 int main()
 {
-	sf::Window window(sf::VideoMode(800, 600), "Avengers");
+	sf::RenderWindow window(sf::VideoMode(1370, 770), "Avengers");
 	window.setFramerateLimit(60);
+
+	float shipx = 47, shipy = 331;
+
 	sf::Texture background;
-	background.loadFromFile("background.png");
-	sf::Sprite sprite;
-	sprite.setTexture(background);
+	background.loadFromFile("background.jpg");
+
+	sf::Texture spaceship;
+	spaceship.loadFromFile("spaceship.png");
+	
+
+	sf::Sprite bk;
+	bk.setTexture(background);
+
+	sf::Sprite ss;
+	ss.setTexture(spaceship);
+
+	ss.setPosition(sf::Vector2f(shipx, shipy));
+
+	
+
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -24,10 +40,25 @@ int main()
 				window.close();
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
 				window.close();
+			
+			
 		}
 
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+			shipy = shipy - 5;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+			shipy = shipy + 5;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+			shipx = shipx - 5;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+			shipx = shipx + 5;
+
+		ss.setPosition(sf::Vector2f(shipx, shipy));
+		
+
 		window.clear();
-		window.draw(sprite);
+		window.draw(bk);
+		window.draw(ss);
 		window.display();
 
 	}
