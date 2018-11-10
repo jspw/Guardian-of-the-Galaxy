1703c621579772fe0c01059d584219b9647aceac
Vampire_5 <mhshifat757@gmail.com>
2018-11-10 19:52:19 +0600

    40% submission

--- GA/Orbitanium2/UIController.h
+++
@@ -1,9 +0,0 @@
-#ifndef UI_CONTROLLER_H
-#define UI_CONTROLLER_H
-
-	int curentMarkedButton; 
-	int curentSelectedMenu = -1; 
-
-	void SetMarked(int _number){curentMarkedButton = _number;}
-	void SetSelected(int _number){curentSelectedMenu = _number;}
-#endif
